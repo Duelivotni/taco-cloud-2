6911ad56ffd60ce6edc0d414161835189e05fa76
Duelivotni <souji07031988@gmail.com>
2021-09-17 03:28:53 +0500

    pom.xml restored to the previous & DesignTacoController fixed

--- taco-cloud-2/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud-2/src/main/java/tacos/web/DesignTacoController.java
@@ -3,18 +3,17 @@ package tacos.web;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import lombok.extern.slf4j.Slf4j;
-
-import tacos.Taco;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Taco;
 
 @Slf4j
 @Controller
@@ -43,5 +42,13 @@ public class DesignTacoController {
         model.addAttribute("design", new Taco());
 
         return "design";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+
+        return ingredients.stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    
     }
 }
