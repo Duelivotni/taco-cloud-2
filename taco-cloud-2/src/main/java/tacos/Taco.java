5fb9408179cc29b4866225f30d294b654d81a9a8
Duelivotni <souji07031988@gmail.com>
2021-10-08 01:52:10 +0500

    Validation for Taco and Taco Order

--- taco-cloud-2/src/main/java/tacos/Taco.java
+++ taco-cloud-2/src/main/java/tacos/Taco.java
@@ -1,10 +1,18 @@
 package tacos;
 
 import java.util.List;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Taco {
+
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters")
     private String name;
+
+    @NotNull
+    @Size(min = 1, message = "There must be at least 1 ingredient in your Taco")
     private List<Ingredient> ingredients;
 }
--- taco-cloud-2/src/main/java/tacos/TacoOrder.java
+++ taco-cloud-2/src/main/java/tacos/TacoOrder.java
@@ -2,17 +2,40 @@ package tacos;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
 import lombok.Data;
 
 @Data
 public class TacoOrder {
+
+    @NotBlank
     private String deliveryName;
+
+    @NotBlank
     private String deliveryStreet;
+
+    @NotBlank
     private String deliveryCity;
+
+    @NotBlank
     private String deliveryState;
+
+    @NotBlank
     private String deliveryZip;
+
+    @CreditCardNumber(message = "The card number is incorrect")
     private String ccNumber;
+
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message="Must be formatted MM/YY")
     private String ccExpiration;
+
+    @Digits(integer = 3, fraction = 0)
     private String ccVV;
     
     private List<Taco> tacos = new ArrayList<>();
