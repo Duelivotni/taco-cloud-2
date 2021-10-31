79080dafdb01ef787d643ad483a9cbb8427c92f8
Duelivotni <souji07031988@gmail.com>
2021-11-01 00:36:51 +0500

    Ingredient data via the repo

--- taco-cloud-2/src/main/java/tacos/data/JdbcIngredientRepository.java
+++ taco-cloud-2/src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -8,9 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
-
 import javax.swing.plaf.ListUI;
-
 import com.google.common.util.concurrent.ServiceManager.Listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +35,13 @@ public class JdbcIngredientRepository implements IngredientRepository {
         List<Ingredient> result = jdbcTemplate.query(
             "select id, name, type from Ingredient where id=?", this :: mapRowToIngredient, id);
         return result.size() == 0 ? Optional.empty() : Optional.of(result.get(0));    
+    }
+
+    @Override
+    public Ingredient save(Ingredient ingredient) {
+        jdbcTemplate.update("insert into Ingredient(id, name, type) values (?, ?, ?)",
+        ingredient.getId(), ingredient.getName(), ingredient.getType().toString());
+        return ingredient;
     }
 
     private Ingredient mapRowToIngredient(ResultSet row, int rowNum) throws SQLException {
--- taco-cloud-2/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud-2/src/main/java/tacos/web/DesignTacoController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 import com.google.inject.internal.Errors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.data.IngredientRepository;
 import tacos.Taco;
 
 @Slf4j
@@ -23,21 +27,29 @@ import tacos.Taco;
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 
+    IngredientRepository ingredientRepository;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-            new Ingredient("CHED", "Cheddar", Type.CHEESE),
-            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-            new Ingredient("SLSA", "Salsa", Type.SAUCE),
-            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)    
-        );
+        // List<Ingredient> ingredients = Arrays.asList(
+        //     new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+        //     new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+        //     new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+        //     new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+        //     new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+        //     new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+        //     new Ingredient("CHED", "Cheddar", Type.CHEESE),
+        //     new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+        //     new Ingredient("SLSA", "Salsa", Type.SAUCE),
+        //     new Ingredient("SRCR", "Sour Cream", Type.SAUCE)    
+        // );
 
+        Iterable <Ingredient> ingredients = ingredientRepository.findAll();
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
